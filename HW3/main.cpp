a312be776ebb3b10dcba37ba1c98e0d4b4dd8dad
nthu108010018 <henry.hung.taiwan@gapp.nthu.edu.tw>
2021-05-08 10:21:48 -0700

    update

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -20,9 +20,9 @@
 #define PI 3.1415
     
 const char* host = "192.168.177.124";
+int flag_1 = 0;
+int flag_2 = 0;
 
-
-WiFiInterface *wifi;
 
 volatile int message_num = 0;
 
@@ -79,12 +79,93 @@ RPCFunction modelrun(&model_run, "model_run");
 RPCFunction Angle(&rpcANGLE, "angle_detect");
 RPCFunction Close(&rpcClose, "close");
 
+WiFiInterface *wifi = WiFiInterface::get_default_instance();
 
+NetworkInterface* net = wifi;
+
+MQTTNetwork mqttNetwork(net);
+
+MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
 
 
 int main(int argc, char* argv[]) {
 
+  
+   
+
+    if (!wifi) {
+
+            printf("ERROR: No WiFiInterface found.\r\n");
+
+            return -1;
+
+    }
+
+
+
+    printf("\nConnecting to %s...\r\n", MBED_CONF_APP_WIFI_SSID);
+
+    int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
+
+    if (ret != 0) {
+
+            printf("\nConnection error: %d\r\n", ret);
+
+            return -1;
+
+    }
+
+
+
     
+
+    //TODO: revise host to your IP
+
+
+    printf("Connecting to TCP network...\r\n");
+
+
+    SocketAddress sockAddr;
+
+    sockAddr.set_ip_address(host);
+
+    sockAddr.set_port(1883);
+
+
+    printf("address is %s/%d\r\n", (sockAddr.get_ip_address() ? sockAddr.get_ip_address() : "None"),  (sockAddr.get_port() ? sockAddr.get_port() : 0) ); //check setting
+
+
+    int rc = mqttNetwork.connect(sockAddr);//(host, 1883);
+
+    if (rc != 0) {
+
+            printf("Connection error.");
+
+            return -1;
+
+    }
+
+    printf("Successfully connected!\r\n");
+
+
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+
+    data.MQTTVersion = 3;
+
+    data.clientID.cstring = "Mbed";
+
+
+    if ((rc = client.connect(data)) != 0){
+
+            printf("Fail to connect MQTT\r\n");
+
+    }
+
+    if (client.subscribe(topic, MQTT::QOS0, messageArrived) != 0){
+
+            printf("Fail to subscribe\r\n");
+
+    }
     char buf[256], outbuf[256];
 
 
@@ -194,86 +275,7 @@ int PredictGesture(float* output) {
 }
 
 int model_deploy(){
-      wifi = WiFiInterface::get_default_instance();
-
-    if (!wifi) {
-
-            printf("ERROR: No WiFiInterface found.\r\n");
-
-            return -1;
-
-    }
-
-
-
-    printf("\nConnecting to %s...\r\n", MBED_CONF_APP_WIFI_SSID);
-
-    int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
-
-    if (ret != 0) {
-
-            printf("\nConnection error: %d\r\n", ret);
-
-            return -1;
-
-    }
-
-
-
-    NetworkInterface* net = wifi;
-
-    MQTTNetwork mqttNetwork(net);
-
-    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
-
-
-    //TODO: revise host to your IP
-
-
-    printf("Connecting to TCP network...\r\n");
-
-
-    SocketAddress sockAddr;
-
-    sockAddr.set_ip_address(host);
-
-    sockAddr.set_port(1883);
-
-
-    printf("address is %s/%d\r\n", (sockAddr.get_ip_address() ? sockAddr.get_ip_address() : "None"),  (sockAddr.get_port() ? sockAddr.get_port() : 0) ); //check setting
-
-
-    int rc = mqttNetwork.connect(sockAddr);//(host, 1883);
-
-    if (rc != 0) {
-
-            printf("Connection error.");
-
-            return -1;
-
-    }
-
-    printf("Successfully connected!\r\n");
-
-
-    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-
-    data.MQTTVersion = 3;
-
-    data.clientID.cstring = "Mbed";
-
-
-    if ((rc = client.connect(data)) != 0){
-
-            printf("Fail to connect MQTT\r\n");
-
-    }
-
-    if (client.subscribe(topic, MQTT::QOS0, messageArrived) != 0){
-
-            printf("Fail to subscribe\r\n");
-
-    }
+    
     
 
   // Whether we should clear the buffer next time we fetch data
@@ -325,7 +327,7 @@ int model_deploy(){
   // incur some penalty in code space for op implementations that are not
 
   // needed by this graph.
-
+  if(!flag_1){
   static tflite::MicroOpResolver<6> micro_op_resolver;
 
   micro_op_resolver.AddBuiltin(
@@ -405,7 +407,7 @@ int model_deploy(){
   error_reporter->Report("Set up successful...\n");
 
 
-  while (true) {
+  while (!flag_1) {
 
 
     // Attempt to read new data from the accelerometer
@@ -466,94 +468,18 @@ int model_deploy(){
 
   
   }
+  }
+     
+    
 }
 
 void model_run(Arguments *in, Reply *out){
+    
     gui_queue.call(&model_deploy);
-        gui_thread.start(callback(&gui_queue, &EventQueue::dispatch_forever));
+    gui_thread.start(callback(&gui_queue, &EventQueue::dispatch_forever));
 }
 
 int Angle_detect(float threshold_angle){
-    wifi = WiFiInterface::get_default_instance();
-
-    if (!wifi) {
-
-            printf("ERROR: No WiFiInterface found.\r\n");
-
-            return -1;
-
-    }
-
-
-
-    printf("\nConnecting to %s...\r\n", MBED_CONF_APP_WIFI_SSID);
-
-    int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
-
-    if (ret != 0) {
-
-            printf("\nConnection error: %d\r\n", ret);
-
-            return -1;
-
-    }
-
-
-
-    NetworkInterface* net = wifi;
-
-    MQTTNetwork mqttNetwork(net);
-
-    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
-
-
-    //TODO: revise host to your IP
-
-
-    printf("Connecting to TCP network...\r\n");
-
-
-    SocketAddress sockAddr;
-
-    sockAddr.set_ip_address(host);
-
-    sockAddr.set_port(1883);
-
-
-    printf("address is %s/%d\r\n", (sockAddr.get_ip_address() ? sockAddr.get_ip_address() : "None"),  (sockAddr.get_port() ? sockAddr.get_port() : 0) ); //check setting
-
-
-    int rc = mqttNetwork.connect(sockAddr);//(host, 1883);
-
-    if (rc != 0) {
-
-            printf("Connection error.");
-
-            return -1;
-
-    }
-
-    printf("Successfully connected!\r\n");
-
-
-    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-
-    data.MQTTVersion = 3;
-
-    data.clientID.cstring = "Mbed";
-
-
-    if ((rc = client.connect(data)) != 0){
-
-            printf("Fail to connect MQTT\r\n");
-
-    }
-
-    if (client.subscribe(topic, MQTT::QOS0, messageArrived) != 0){
-
-            printf("Fail to subscribe\r\n");
-
-    }
     
     while(1){
     BSP_ACCELERO_Init();
@@ -683,6 +609,6 @@ void close_mqtt() {
 }
 
 void rpcClose(Arguments *in, Reply *out){
-    gui_thread.terminate();
-    //angle_thread.terminate();
+    flag_2=1;
+    flag_1=1;
 }
